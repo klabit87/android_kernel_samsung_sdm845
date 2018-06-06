f76ec514a24db86cd5cd40be8a07af2a4de731ad
Michael Benedict <michaelbt@live.com>
2018-06-07 01:54:25 +1000

    net: ipv4: only use when knox_ncm is enabled
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -409,6 +409,7 @@ out_rcu_unlock:
 	goto out;
 }
 
+#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 /** The function is used to check if the ncm feature is enabled or not; if enabled then it calls knox_collect_socket_data function in ncm.c to record all the socket data; **/
 static void knox_collect_metadata(struct socket *sock) {
@@ -417,6 +418,7 @@ static void knox_collect_metadata(struct socket *sock) {
 	}
 }
 /* END_OF_KNOX_NPA */
+#endif
 
 /*
  *	The peer socket should always be NULL (or else). When we call this
@@ -447,9 +449,11 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 			!(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		knox_collect_metadata(sock);
 		/* END_OF_KNOX_NPA */
+#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
