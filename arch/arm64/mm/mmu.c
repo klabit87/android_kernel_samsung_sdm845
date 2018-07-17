5be9ccc92d73b8b321b927d2a75b974c25d85045
Michael Benedict <michaelbt@live.com>
2018-07-17 01:00:17 -0700

    mm: fix undeclared function when rkp is disabled
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -578,10 +578,8 @@ static int __init map_entry_trampoline(void)
 
 	/* Map only the text into the trampoline page table */
 	memset(tramp_pg_dir, 0, PGD_SIZE);
-	rkp_ro_buf_ready = 0;
 	__create_pgd_mapping(tramp_pg_dir, pa_start, TRAMP_VALIAS, PAGE_SIZE,
 			     prot, pgd_pgtable_alloc, 0);
-	rkp_ro_buf_ready = 1;
 
 	/* Map both the text and data into the kernel page table */
 	__set_fixmap(FIX_ENTRY_TRAMP_TEXT, pa_start, prot);
