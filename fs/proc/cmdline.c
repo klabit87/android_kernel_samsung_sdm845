009b577b0bd8d9e99a7dde1da19a4d7718acc3a3
Lukas Berger <mail@lukasberger.at>
2018-08-13 01:15:36 +0400

    Proc: Fake CMDline

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char updated_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", updated_command_line);
 	return 0;
 }
 
@@ -21,38 +24,29 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-static void remove_flag(char *cmd, const char *flag)
+static void proc_cmdline_set(char *name, char *value)
 {
-	char *start_addr, *end_addr;
-
-	/* Ensure all instances of a flag are removed */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memmove(start_addr, end_addr + 1, strlen(end_addr));
-		else
-			*(start_addr - 1) = '\0';
+	char flag_str[COMMAND_LINE_SIZE];
+	char *flag_substr;
+	char *flag_space_substr;
+ 	scnprintf(flag_str, COMMAND_LINE_SIZE, "%s=", name);
+	flag_substr = strstr(updated_command_line, flag_str);
+ 	if (flag_substr) {
+		flag_space_substr = strchr(flag_substr, ' ');
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*s%s", (int)(flag_substr - updated_command_line), updated_command_line, flag_space_substr + 1);
 	}
-}
-
-static void remove_safetynet_flags(char *cmd)
-{
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
-	remove_flag(cmd, "androidboot.veritymode=");
+ 	// flag was not found, insert it
+	scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
 }
 
 static int __init proc_cmdline_init(void)
 {
-	strcpy(new_command_line, saved_command_line);
+		// copy it only once
+	strcpy(updated_command_line, saved_command_line);
+ 	proc_cmdline_set("androidboot.verifiedbootstate", "green");
+	proc_cmdline_set("androidboot.warranty_bit", "0");
+	proc_cmdline_set("androidboot.fmp_config", "1");
 
-	/*
-	 * Remove various flags from command line seen by userspace in order to
-	 * pass SafetyNet CTS check.
-	 */
-	remove_safetynet_flags(new_command_line);
-	
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
