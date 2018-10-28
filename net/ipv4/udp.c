9f0ca957ef87c23368533a2af7fe7159f64836a1
Michael Benedict <michaelbt@live.com>
2018-10-28 15:16:58 +1100

    tree: remove leftover knox function
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -115,9 +115,11 @@
 #include "udp_impl.h"
 #include <net/sock_reuseport.h>
 #include <net/addrconf.h>
+#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 #include <net/ncm.h>
 /* END_OF_KNOX_NPA */
+#endif CONFIG_KNOX_NCM
 
 struct udp_table udp_table __read_mostly;
 EXPORT_SYMBOL(udp_table);
@@ -1777,15 +1779,18 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	if (sk) {
 		struct dst_entry *dst = skb_dst(skb);
 		int ret;
+		#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		struct nf_conn *ct = NULL;
 		enum ip_conntrack_info ctinfo;
 		struct nf_conntrack_tuple *tuple = NULL;
 		/* END_OF_KNOX_NPA */
+		#endif CONFIG_KNOX_NCM
 
 		if (unlikely(sk->sk_rx_dst != dst))
 			udp_sk_rx_dst_set(sk, dst);
 
+		#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		/* function to handle open flows with incoming udp packets */
 		if (check_ncm_flag()) {
@@ -1812,6 +1817,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 			}
 		}
 		/* END_OF_KNOX_NPA */
+		#endif CONFIG_KNOX_NCM
 
 		ret = udp_queue_rcv_skb(sk, skb);
 		sock_put(sk);
@@ -1830,16 +1836,19 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	sk = __udp4_lib_lookup_skb(skb, uh->source, uh->dest, udptable);
 	if (sk) {
 		int ret;
+		#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		struct nf_conn *ct = NULL;
 		enum ip_conntrack_info ctinfo;
 		struct nf_conntrack_tuple *tuple = NULL;
 		/* END_OF_KNOX_NPA */
+		#endif CONFIG_KNOX_NCM
 
 		if (inet_get_convert_csum(sk) && uh->check && !IS_UDPLITE(sk))
 			skb_checksum_try_convert(skb, IPPROTO_UDP, uh->check,
 						 inet_compute_pseudo);
 
+		#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		/* function to handle open flows with incoming udp packets */
 		if (check_ncm_flag()) {
@@ -1866,6 +1875,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 			}
 		}
 		/* END_OF_KNOX_NPA */
+		#endif CONFIG_KNOX_NCM
 
 		ret = udp_queue_rcv_skb(sk, skb);
 
--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -56,9 +56,11 @@
 #include <net/netfilter/nf_nat_helper.h>
 #include <net/netns/hash.h>
 
+#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 #include <net/ncm.h>
 /* END_OF_KNOX_NPA */
+#endif CONFIG_KNOX_NCM
 
 #define NF_CONNTRACK_VERSION	"0.5.0"
 
@@ -332,12 +334,14 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 {
 	struct ct_pcpu *pcpu;
 
+	#ifdef CONFIG_KNOX_NCM
     /* START_OF_KNOX_NPA */
     /* send dying conntrack entry to collect data */
     if ( (check_ncm_flag()) && (ct != NULL) && (atomic_read(&ct->startFlow)) ) {
         knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_CLOSE, 10);
     }
     /* END_OF_KNOX_NPA */
+	#endif CONFIG_KNOX_NCM
 
 	/* add this conntrack to the (per cpu) dying list */
 	ct->cpu = smp_processor_id();
@@ -1089,9 +1093,11 @@ __nf_conntrack_alloc(struct net *net,
 		     gfp_t gfp, u32 hash)
 {
 	struct nf_conn *ct;
+	#ifdef CONFIG_KNOX_NCM
 	/* START_OF_KNOX_NPA */
 	struct timespec open_timespec;
 	/* END_OF_KNOX_NPA */
+	#endif CONFIG_KNOX_NCM
 
 	/* We don't want any race condition at early drop stage */
 	atomic_inc(&net->ct.count);
@@ -1114,6 +1120,7 @@ __nf_conntrack_alloc(struct net *net,
 		goto out;
 
 	spin_lock_init(&ct->lock);
+	#ifdef CONFIG_KNOX_NCM
 	/* START_OF_KNOX_NPA */
 	/* initialize the conntrack structure members when memory is allocated */
 	if (ct != NULL) {
@@ -1132,6 +1139,7 @@ __nf_conntrack_alloc(struct net *net,
 		atomic_set(&ct->startFlow, 0);
 	}
 	/* END_OF_KNOX_NPA */
+	#endif CONFIG_KNOX_NCM
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple = *orig;
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].hnnode.pprev = NULL;
 	ct->tuplehash[IP_CT_DIR_REPLY].tuple = *repl;
