16a1f84e0c7363f5de209d67cd85c061aeb8cc08
klabit87 <keith.labit@gmail.com>
2019-07-19 14:26:59 -0500

    OSRC: G9650ZCU5CSFB

--- drivers/crypto/msm/ice.c
+++ drivers/crypto/msm/ice.c
@@ -1804,7 +1804,7 @@ int qcom_ice_setup_ice_hw(const char *storage_type, int enable)
 	if (ice_dev == ERR_PTR(-EPROBE_DEFER))
 		return -EPROBE_DEFER;
 
-	if (!ice_dev)
+	if (!ice_dev || (ice_dev->is_ice_enabled == false))
 		return ret;
 
 	if (enable)
