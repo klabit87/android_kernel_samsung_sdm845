b548f9960f51948c0af03e5ca203392ff2016179
klabit87 <keith.labit@gmail.com>
2020-07-11 08:43:04 -0500

    OSRC: G9650ZHS6DTF1

--- include/linux/ologk.h
+++ include/linux/ologk.h
@@ -2,10 +2,12 @@
 #define _OLOG_KERNEL_H_
 
 #include <linux/unistd.h>
+#include "olog.pb.h"
 
 #define OLOG_CPU_FREQ_FILTER   1500000
 
-#define ologk(...)
-#define perflog(...)
+
+#define ologk(...) perflog(PERFLOG_UNKNOWN, __VA_ARGS__)
+extern void perflog(int type, const char *fmt, ...);
 
 #endif
