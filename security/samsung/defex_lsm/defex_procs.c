cb834cd5c11c48ea9d71818edaa596f176695d0a
klabit87 <keith.labit@gmail.com>
2020-11-15 13:48:35 -0600

    OSRC: G9650ZHU7ETI3

--- security/samsung/defex_lsm/defex_procs.c
+++ security/samsung/defex_lsm/defex_procs.c
@@ -255,7 +255,9 @@ out:
 
 /* Cred. violation feature decision function */
 #define AID_MEDIA_RW	1023
-#define SECUREFD_MEDIA_RW	0xE4E5BF
+#define AID_MEDIA_OBB	1059
+#define AID_SYSTEM	1000
+
 static int task_defex_check_creds(struct defex_context *dc)
 {
 	char *path = NULL;
@@ -343,8 +345,9 @@ static int task_defex_check_creds(struct defex_context *dc)
 	 			(cur_egid != ref_egid) ||
 	  			!((cur_fsuid == ref_fsuid) ||
 	  			 (cur_fsuid == ref_uid) ||
-	  			 (cur_fsuid == AID_MEDIA_RW) ||
-	  			 (cur_fsuid == SECUREFD_MEDIA_RW)) ) {
+	  			 (cur_fsuid%100000 == AID_SYSTEM) ||
+	  			 (cur_fsuid%100000 == AID_MEDIA_RW) ||
+	  			 (cur_fsuid%100000 == AID_MEDIA_OBB)) ) {
 			check_deeper = 1;
 			if (CHECK_ROOT_CREDS(p))
 				set_task_creds(REF_PID(p), 1, 1, 1, cred_flags);
