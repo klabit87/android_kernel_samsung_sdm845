880d9b88a19a7e8009cffb1eecd24d457839a77a
klabit87 <keith.labit@gmail.com>
2021-09-25 14:09:18 -0500

    OSRC: G960USQU9FUE1

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,7 @@ export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-and
 
 mkdir out
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android star2qlte_usa_single_defconfig
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android starqlte_usa_single_defconfig
 
 
 make -j64 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
